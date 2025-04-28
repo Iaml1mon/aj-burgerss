ec12fc6fed8e0c2dca9978c440b84e6676f04727
Iaml1mon <75097283+Iaml1mon@users.noreply.github.com>
2025-04-29 08:52:57 +1000

    Update Contact.tsx

--- src/components/home/Contact.tsx
+++ src/components/home/Contact.tsx
@@ -86,7 +86,7 @@ const Contact = () => {
     setErrors({}); // Clear previous errors
 
     // Updated Google Apps Script Web App URL provided by the user
-    const scriptURL = "https://script.google.com/macros/s/AKfycbxsZFwOARqJG8goIgl3SN_Fl_3ajQb1nCeKjdyFoCevV94P1ENEE_SfTSMKhfbcqP-8/exec";
+    const scriptURL = "https://script.google.com/macros/s/AKfycbzmMymu62H9bohvk9pnvmQPZp_mk4nYgbY5F1CHXpuvGZ-Ucot3cH3OpBg3OiEZLAWk/exec";
 
     try {
       await fetch(scriptURL, {
